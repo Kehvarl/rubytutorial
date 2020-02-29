c88081b0145f5c3ae42789887336395e991f691c
Adam Corbett <kehvarl@gmail.com>
2020-02-29 09:26:24 -0700

    Ruby classes
    @@varname - class variable (not recommended style)
    
    @varname - instance variable (seems to be only available within class)
    
    varname - local variable (only within method)

--- test.rb
+++ test.rb
@@ -1,5 +1,19 @@
 #!/usr/bin/ruby -w
 
+class Customer
+  def initialize(id, name, addr)
+    @cust_id = id
+    @cust_name = name
+    @cust_addr = addr
+  end
+
+  def display_details
+    puts "Customer id: #@cust_id"
+    puts "Customer name:  #@cust_name"
+    puts "Customer address:  #@cust_addr"
+  end
+
+end
 
 END {
   puts "Terminating Ruby Program."
@@ -9,4 +23,9 @@ BEGIN {
   puts "Initializing Ruby Program."
 }
 
-puts "Hello, Ruby!"
+cust1 = Customer.new(1, "one", "here")
+cust2 = Customer.new(2, "two", "there")
+
+puts cust1.display_details
+puts cust2.display_details
+
