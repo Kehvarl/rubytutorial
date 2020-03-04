7359eae91449e171325505b2c5a04ff57278ff72
Adam Corbett <kehvarl@gmail.com>
2020-03-03 21:57:58 -0700

    set up map

--- creature.rb
+++ creature.rb
@@ -1,5 +1,22 @@
 class Creature
   def initialize(x,y)
       @pos_x, @pos_y = x, y
+      @moves = Array.new(8, 1)
   end
 end
+
+class World
+  def initialize(w, h)
+    @width, @height = w, h
+    @game_map = Array.new(w * h, 0)
+  end
+
+  def get_tile(x, y)
+    @game_map[self.get_tile_address(x, y)]
+  end
+
+  def get_tile_address(x, y)
+    x + y * @width
+  end
+end
+
